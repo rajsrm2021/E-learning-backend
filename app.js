3441b0a97d843f673fa4c05e55bc4dbbc539b665
rajsrm2021 <rj9769@srmist.edu.in>
2023-09-17 17:08:51 +0530

    bug fixed

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ config({
 });
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with the origin(s) you want to allow
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies and authentication headers
+}));
+
 
 // using middlerwares
 
@@ -40,10 +46,21 @@ app.use("/api/v1", other);
 
 export default app;
 
-app.get("/", (req, res) =>
+// app.get("/", (req, res) =>
+//   res.send(
+//     `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+//   )
+  
+// );
+
+app.get("/", (req, res) => {
+  const frontendURL = "http://localhost:3000";
+  // const frontendURL = "https://e-learning-frontend-virid.vercel.app/";
+
   res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
+    `<h1>Site is Working. Click <a href="${frontendURL}">here</a> to visit frontend.</h1>`
+  );
+});
+
 
 app.use(ErrorMiddleware); // suun lo meri baat isko hamesa last me call krna hai bas !!!!
